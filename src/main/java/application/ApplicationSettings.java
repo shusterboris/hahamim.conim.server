334edd8ce9cc6ba2e675d683bbf5bc11c2188b47
Boris Shuster <shuster.boris@gmail.com>
2020-10-20 20:54:05 +0300

    minor changes add hostServer / hostPort to application.properties and get it from ApplicationSettings

--- src/main/java/application/ApplicationSettings.java
+++ src/main/java/application/ApplicationSettings.java
@@ -1,5 +1,15 @@
 package application;
 
-public class ApplicationSettings {
+import org.springframework.beans.factory.annotation.Value;
 
+public class ApplicationSettings {
+	@Value("${app.host.addr}")
+	private static String hostServer;
+
+	@Value("${app.host.port}")
+	private static String hostPort;
+
+	public static final String getHost() {
+		return hostServer.concat(":").concat(hostPort);
+	}
 }
