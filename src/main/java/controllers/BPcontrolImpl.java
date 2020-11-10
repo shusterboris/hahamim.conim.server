342d780743549e87b9ef0fdafc73f208180f6ea7
innas2019 <inna.i.shuster@gmail.com>
2020-11-10 19:12:29 +0200

    businessPartner service

---
+++ src/main/java/controllers/BPcontrolImpl.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import proxies.BusinessPartner;
+import proxies.Proposal;
+import services.MockService;
+
+
+@RestController
+public class BPcontrolImpl implements BPcontrol {
+	private MockService mService = new MockService();
+	
+	@Override
+	public ResponseEntity<Object> getAllPartners() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Object> getPartnerById(Long id) {
+		try {
+			BusinessPartner res = mService.getPartnerById(id);
+			return new ResponseEntity<Object>(res, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>("Server error:".concat(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
