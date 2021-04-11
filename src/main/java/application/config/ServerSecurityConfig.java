f33d959c063b07bf781cbdcdffeddae8e3d813ec
shusterboris <shuster.boris@gmail.com>
2021-04-11 11:07:53 +0300

    Исправлена ошибка - показ списка акций требует авторизации Убран подробный протокол SQL запросов

--- src/main/java/application/config/ServerSecurityConfig.java
+++ src/main/java/application/config/ServerSecurityConfig.java
@@ -1,5 +1,7 @@
 package application.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -71,5 +76,15 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
--- src/main/java/application/controllers/ActionsControlImpl.java
+++ src/main/java/application/controllers/ActionsControlImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
@@ -36,6 +37,7 @@ import proxies.Proposal;
 import proxies.Purchase;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ActionsControlImpl implements ActionsControl {
 	// private MockService mService = new MockService();
 	@Autowired
